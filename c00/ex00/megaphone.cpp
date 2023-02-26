061cfbcda2bc112be79e4b9a1edcf836d0234c23
hu8813 <h0553559@gmail.com>
2023-02-26 11:56:10 +0100

    a

--- c00/ex00/megaphone.cpp
+++ c00/ex00/megaphone.cpp
@@ -2,26 +2,23 @@
 #include <string>
 #include <cctype>
 
-int main(int argc, char** argv) {
-  if (argc == 1) {
-    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg(argv[i]);
+        for (std::string::iterator it = arg.begin(); it != arg.end(); ++it) {
+            *it = std::toupper(*it);
+        }
+        std::cout << arg;
+        if (i != argc - 1) {
+            std::cout << ' ';
+        }
+    }
+    std::cout << std::endl;
+
     return 0;
-    }
-  
-  for (int i = 1; i < argc; i++) {
-    std::string arg = argv[i];
-
-    for (auto& c : arg) {
-      c = std::toupper(c);
-    }
-
-    std::cout << arg;
-    
-    if (i < argc - 1) {
-      std::cout << " ";
-    }
-  }
-  std::cout << std::endl;
-
-  return 0;
 }
