c8fe8ad48ae2f5b4b9a122d5033f70ae1d7ae322
huaydin <38990435+hu8813@users.noreply.github.com>
2023-02-26 11:42:45 +0100

    Update megaphone.cpp

--- c00/ex00/megaphone.cpp
+++ c00/ex00/megaphone.cpp
@@ -3,6 +3,11 @@
 #include <cctype>
 
 int main(int argc, char** argv) {
+  if (argc == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+    return 0;
+    }
+  
   for (int i = 1; i < argc; i++) {
     std::string arg = argv[i];
 
