4fbc36a1e1d46d6c47456ffde482f4f2272efb92
hu8813 <h0553559@gmail.com>
2023-02-27 21:18:18 +0100

    a

--- c00/ex00/megaphone.cpp
+++ c00/ex00/megaphone.cpp
@@ -11,12 +11,9 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
         for (std::string::iterator it = arg.begin(); it != arg.end(); ++it) {
-            *it = std::toupper(*it);
+            *it = std::toupper(static_cast<unsigned char>(*it));
         }
         std::cout << arg;
-        if (i != argc - 1) {
-            std::cout << ' ';
-        }
     }
     std::cout << std::endl;
 
