d98ca35538aa3b3e25f753ede2d6f21ff784c150
Huseyin Kaya Aydin <huaydin@c1r1p8.42vienna.com>
2023-03-03 19:38:03 +0100

    a

--- c01/ex05/Harl.cpp
+++ c01/ex05/Harl.cpp
@@ -21,24 +21,28 @@ std::map<std::string, void (Harl::*)()> Harl::init_funcs() {
 
 
 void Harl::debug() {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-"
 		"ketchup burger. I really do!" << std::endl;
     
 }
 
 void Harl::info() {
-   std::cout << "I cannot believe adding extra bacon costs more money." 
+	std::cout << "[ INFO ]" << std::endl;
+   	std::cout << "I cannot believe adding extra bacon costs more money." 
 		"You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
     
 }
 
 void Harl::warning() {
+	std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free." 
 		"I've been coming for years whereas you started working here since last month." << std::endl;
     
 }
 
 void Harl::error() {
+	std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
     
 }
