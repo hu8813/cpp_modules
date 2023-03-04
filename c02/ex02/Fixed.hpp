84647e34e2c5fc64d55b70c7fe4251c9743efaed
Huseyin Kaya Aydin <huaydin@c1r1p8.42vienna.com>
2023-03-04 16:21:07 +0100

    a

---
+++ c02/ex02/Fixed.hpp
@@ -0,0 +1,56 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class Fixed
+{
+	public:
+		// Constructors
+		Fixed();
+		Fixed(int intNumber);
+		Fixed(float floatNumber);
+		Fixed(const Fixed &copy);
+		
+		// Destructor
+		~Fixed();
+		
+		// Operators
+		Fixed & operator=(const Fixed &assign);
+		bool operator>(const Fixed &rhs) const;
+		bool operator<(const Fixed &rhs) const;
+		bool operator>=(const Fixed &rhs) const;
+		bool operator<=(const Fixed &rhs) const;
+		bool operator==(const Fixed &rhs) const;
+		bool operator!=(const Fixed &rhs) const;
+		Fixed operator+(const Fixed &rhs) const;
+		Fixed operator-(const Fixed &rhs) const;
+		Fixed operator*(const Fixed &rhs) const;
+		Fixed operator/(const Fixed &rhs) const;
+		Fixed & operator++();
+		Fixed operator++(int);
+		Fixed & operator--(); 
+		Fixed operator--(int); 
+		
+		// Getters / Setters
+		int getRawBits() const;
+		void setRawBits(int const raw);
+		int toInt() const;
+		float toFloat( void ) const;
+
+		static Fixed & min(Fixed &a, Fixed &b);
+		static const Fixed & min(const Fixed &a, const Fixed &b);
+		static Fixed & max(Fixed &a, Fixed &b);
+		static const Fixed & max(const Fixed &a, const Fixed &b);
+		
+		
+	private:
+		int _rawBits;
+		static const int _bits = 8;
+		
+};
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+
+#endif
