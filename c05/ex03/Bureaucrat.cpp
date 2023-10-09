fdc4db415a107824a7b4c508fc5cd4ec8496a68b
huaydin <38990435+hu8813@users.noreply.github.com>
2023-10-09 20:49:57 +0000

    a

--- c05/ex03/Bureaucrat.cpp
+++ c05/ex03/Bureaucrat.cpp
@@ -1,15 +1,15 @@
 #include "Bureaucrat.hpp"
 
 Bureaucrat::Bureaucrat(): _name("default"), _grade(150) {
-    // constructor implementation
+    // constructor
 }
 
 Bureaucrat::Bureaucrat(std::string name): _name(name), _grade(150) {
-    // constructor implementation
+    // constructor
 }
 
 Bureaucrat::Bureaucrat(int grade) : _name("default") {
-    // constructor implementation
+    // constructor
     if (grade < 1)
         throw GradeTooHighException();
     if (grade > 150)
@@ -18,7 +18,7 @@ Bureaucrat::Bureaucrat(int grade) : _name("default") {
 }
 
 Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name) {
-    // constructor implementation
+    // constructor
     if (grade < 1)
         throw GradeTooHighException();
     if (grade > 150)
@@ -27,23 +27,23 @@ Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name) {
 }
 
 Bureaucrat::Bureaucrat(const Bureaucrat& other) {
-    // copy constructor implementation
+    // copy constructor
     *this = other;
 }
 
 Bureaucrat::~Bureaucrat() {
-    // destructor implementation
+    // destructor
 }
 
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other) {
-    // assignment operator implementation
+    // assignment operator
         this->_name = other.getName();
         this->_grade = other.getGrade();
     return (*this);
 }
 
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& bureaucrat) {
-    // output stream operator implementation
+    // output stream operator
     os << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
     return os;
 }
@@ -57,35 +57,35 @@ void Bureaucrat::signForm(AForm &form) const {
     }
 }
 std::string Bureaucrat::getName(void) const {
-    // implementation for getting the bureaucrat's name
+    // for getting the bureaucrat's name
     return this->_name;
 }
 
 int Bureaucrat::getGrade(void) const {
-    // implementation for getting the bureaucrat's grade
+    // for getting the bureaucrat's grade
     return this->_grade;
 }
 
 void Bureaucrat::incrementGrade() {
-    // implementation for incrementing the bureaucrat's grade
+    // for incrementing the bureaucrat's grade
     if (_grade == 1)
         throw GradeTooHighException();
     _grade--;
 }
 
 void Bureaucrat::decrementGrade() {
-    // implementation for decrementing the bureaucrat's grade
+    // for decrementing the bureaucrat's grade
     if (_grade == 150)
         throw GradeTooLowException();
     _grade++;
 }
 
 const char* Bureaucrat::GradeTooHighException::what() const throw() {
-    // implementation for getting the exception's message
+    // for getting the exception's message
     return "Grade too high";
 }
 
 const char* Bureaucrat::GradeTooLowException::what() const throw() {
-    // implementation for getting the exception's message
+    // for getting the exception's message
     return "Grade too low";
 }
