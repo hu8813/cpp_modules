9f5cdc311fae07c531c48e00715a30fbfa42f788
huaydin <38990435+hu8813@users.noreply.github.com>
2023-10-12 18:30:15 +0000

    a

--- c07/ex02/main.cpp
+++ c07/ex02/main.cpp
@@ -1,10 +1,59 @@
-#include "Array.hpp"
 #include <iostream>
+#include "Array.hpp"
 
-int main()
+#define MAX_VAL 750
+int main(int, char**)
 {
-    // Create an array of integers with size 5
-    Array<int> intArray(5);
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    std::srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = std::rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = std::rand();
+    }
+    delete [] mirror;//
+    return 0;
+}
+{
+        Array<int> intArray(5);
 
     // Set values in the array
     for (unsigned int i = 0; i < intArray.size(); i++)
@@ -43,6 +92,5 @@ int main()
     {
         std::cout << "Exception caught: " << e.what() << std::endl;
     }
-
-    return 0;
+}
 }
