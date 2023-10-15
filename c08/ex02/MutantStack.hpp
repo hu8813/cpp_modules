f6b876a6f5ad237cc18b26f217de7441a80bddf6
huaydin <38990435+hu8813@users.noreply.github.com>
2023-10-15 20:26:55 +0000

    a

--- c08/ex02/MutantStack.hpp
+++ c08/ex02/MutantStack.hpp
@@ -9,6 +9,9 @@ template <typename T>
 class MutantStack : public std::stack<T, std::deque<T> > {
 public:
     typedef typename std::deque<T>::iterator iterator;
+    typedef typename std::deque<T>::const_iterator const_iterator;
+    typedef typename std::deque<T>::reverse_iterator reverse_iterator;
+    typedef typename std::deque<T>::const_reverse_iterator const_reverse_iterator;
 
     iterator begin() {
         return this->c.begin();
@@ -16,5 +19,29 @@ public:
 
     iterator end() {
         return this->c.end();
+    }
+
+    const_iterator begin() const {
+        return this->c.begin();
+    }
+
+    const_iterator end() const {
+        return this->c.end();
+    }
+
+    reverse_iterator rbegin() {
+        return this->c.rbegin();
+    }
+
+    reverse_iterator rend() {
+        return this->c.rend();
+    }
+
+    const_reverse_iterator rbegin() const {
+        return this->c.rbegin();
+    }
+
+    const_reverse_iterator rend() const {
+        return this->c.rend();
     }
 };
