a199c302001cd5e409b00e7cada91b35c5147629
huaydin <38990435+hu8813@users.noreply.github.com>
2023-10-21 08:45:20 +0000

    a

--- c09/ex02/PmergeMe.cpp
+++ c09/ex02/PmergeMe.cpp
@@ -1,7 +1,7 @@
 #include "PmergeMe.hpp"
 
 void mergeInsertSort(std::vector<int>& vec) {
-    if (vec.size() <= 1) return;
+    if (vec.size() < 2) return;
 
     for (size_t i = 1; i < vec.size(); i += 2) {
         if (vec[i] < vec[i-1]) {
@@ -10,18 +10,18 @@ void mergeInsertSort(std::vector<int>& vec) {
     }
 
     for (int i = 1; i < static_cast<int>(vec.size()); ++i) {
-        int key = vec[i];
+        int tmp = vec[i];
         int j = i - 1;
-        while (j >= 0 && vec[j] > key) {
+        while (j >= 0 && vec[j] > tmp) {
             vec[j + 1] = vec[j];
             --j;
         }
-        vec[j + 1] = key;
+        vec[j + 1] = tmp;
     }
 }
 
 void mergeInsertSort(std::list<int>& lst) {
-    if (lst.size() <= 1) return;
+    if (lst.size() < 2) return;
 
     std::list<int>::iterator it = lst.begin();
     std::advance(it, 1);
@@ -31,7 +31,6 @@ void mergeInsertSort(std::list<int>& lst) {
         if (*it < *prev) {
             std::swap(*it, *prev);
         }
-        ++it;
     }
 
     it = lst.begin();
